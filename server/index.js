42b9360ded8a4d67557d488b0d88cf3b1b20d383
Nikhilesh Ratna <101418235+nikhileshratna@users.noreply.github.com>
2024-06-11 19:47:41 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,53 +9,49 @@ const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./config/cloudinary");
+const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
+// Load environment variables from .env file
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-//database connect
+// Connect to the database
 database.connect();
-//middlewares
+
+// Use middlewares
 app.use(express.json());
 app.use(cookieParser());
-// Example in an Express.js server
-app.use(
-	cors({
-		origin:"*",
-		credentials:true,
-	})
-)
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+}));
 
-
-app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
-//cloudinary connection
+// Connect to cloudinary
 cloudinaryConnect();
 
-//routes
+// Define routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-//def route
-
+// Define default route
 app.get("/", (req, res) => {
-	return res.json({
-		success:true,
-		message:'Your server is up and running....'
-	});
+    console.log("Root route accessed");
+    return res.json({
+        success: true,
+        message: "Your server is up and running....",
+    });
 });
 
+// Start server
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
-
+    console.log(`App is running at ${PORT}`);
+});
